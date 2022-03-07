ac04573fcedc68c45a610cb8ad0b4f43529da816
christopherkeibel <christopher.keibel@karakun.com>
2022-03-07 15:10:57 +0100

    Add main (load agent and play)

---
+++ main.py
@@ -0,0 +1,29 @@
+import gym
+from time import sleep
+from wrappers import make_env
+import tensorflow as tf
+import numpy as np
+
+AGENT_LIST = ["suicide_pacman", "good_pacman"]
+GAME_LIST = ["MsPacman-v0", "Pong-v0"]
+EPISODES = 2
+
+
+def load_agent(name: str):
+    PATH = f"model/{name}"
+    agent = tf.keras.models.load_model(PATH)
+    return agent
+
+
+if __name__ == "__main__":
+    agent = load_agent(AGENT_LIST[0])
+    env = make_env(GAME_LIST[0])
+
+    for episode in range(EPISODES):
+        state = env.reset()
+        done = False
+        while not done:
+            action = np.argmax(agent.predict(state))
+            state, reward, done, info = env.step(action)
+            sleep(0.1)
+            env.render()
